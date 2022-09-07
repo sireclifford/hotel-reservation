67cc7daaa3ca7509b845f20070cfb0f726b09fdf
sireclifford <dev.sire.clifford@gmail.com>
2022-09-07 09:55:31 +0000

    create property-list component

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,6 +1,7 @@
 import Navbar from '../../components/navbar/Navbar';
 import Header from '../../components/header/Header';
 import Featured from '../../components/featured/Featured';
+import PropertyList from '../../components/propertyList/PropertyList';
 import './Home.css';
 
 const Home = () => (
@@ -10,7 +11,7 @@ const Home = () => (
     <div className="home-container">
       <Featured />
       <h1 className="home-title">Browse by property type</h1>
-      {/* Property List */}
+      <PropertyList />
       <h1 className="home-title">Homes guests love</h1>
       {/* Featured Properties */}
       {/* Footer */}
